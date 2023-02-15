645ad6fed8c901a5fcc21ae1618c12d7b63a1c4e
kaho <1316156936@qq.com>
2023-02-15 11:40:39 +0800

    build: 项目整合Swagger2

--- service/service_hosp/src/main/java/com/kaho/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/kaho/yygh/hosp/ServiceHospApplication.java
@@ -2,6 +2,7 @@ package com.kaho.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @description: 医院模块主启动类
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2023-02-14 20:54
  **/
 @SpringBootApplication
+@ComponentScan("com.kaho") // 扫描到 common - service_util 下的 Swagger 配置
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
